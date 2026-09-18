f7ded75410fbc2aa26315440304a6877238a639f
devEraldoJunior <eraldo.pessoal01@gmail.com>
2026-09-18 17:55:23 +0000

    Treinamento de CRUD

--- sistema_teste/routes/web.php
+++ sistema_teste/routes/web.php
@@ -2,47 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
+use App\Models\Aluno;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/alunos', function () {
-    return 'Lista de alunos';
-});
+Route::get('/alunos', function (){
+    $alunos = Aluno::all();
 
-Route::get('/professores', function () {
-    return 'Lista de professores';
-});
-
-Route::get('/alunos/{id}/{nome}', function ($id, $nome) {
-    return 'Aluno: ' . $id . '<br>Nome: ' . $nome;
-});
-
-Route::get('/professores/{nome}', function ($nome) {
-    return 'Professor: ' . $nome;
-});
-
-Route::get('/produtos/{id}/{nome}/{preco}', function ($id, $nome, $preco) {
-    return 'Produto: ' . $id . '<br>Nome: ' . $nome . '<br>Preço: ' .$preco;
-});
-
-Route::get('/cadastro', function () {
-    return '
-        <form method="POST" action="/cadastro">
-            <input type="text" name="nome" placeholder="Digite seu nome">
-            <br><br>
-            <input type="number" name="idade" placeholder="Digite sua idade">
-            <br><br>
-            <button type="submit">Cadastrar</button>
-        </form>
-    ';
-});
-
-Route::post('/cadastro', function (Request $request) {
-    return 'Nome recebido: ' . $request->nome . '<br>Idade recebida: ' . $request->idade;
+    return $alunos;
 });
 
 
+Route::get('/excluir-aluno', function () {
+    $aluno = Aluno::find(1);
 
+    $aluno->delete();
+
+    return 'Aluno excluido';
+});
 
